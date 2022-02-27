523622a2bd7f71f2f899efbe8168026a9f6c3c30
Declipsonator <96392121+Declipsonator@users.noreply.github.com>
2022-02-27 16:49:26 -0500

    Add files via upload

---
+++ src/main/java/me/declipsonator/recipeunlocker/recipeunlocker/mixin/RecipeBookWidgetMixin.java
@@ -0,0 +1,22 @@
+package me.declipsonator.recipeunlocker.recipeunlocker.mixin;
+
+import me.declipsonator.recipeunlocker.recipeunlocker.util.GhostSlotClear;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookGhostSlots;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(RecipeBookWidget.class)
+public class RecipeBookWidgetMixin implements GhostSlotClear {
+
+
+	@Shadow
+	@Final
+	protected RecipeBookGhostSlots ghostSlots;
+
+	@Override
+	public void clearGhostSlots() {
+		this.ghostSlots.reset();
+	}
+}
